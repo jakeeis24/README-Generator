fe67ed2d6f4da82e36d5e07ae2675ade1c0e9f0c
Jake Eissler <jakeeissler1@gmail.com>
2022-03-03 23:23:24 -0700

    added the license section with a functional link in the table of contents. The badges are working at the top of the readme. I am now going to test the links in the actual section to see if they work.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,11 +2,11 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === "MIT") {
-    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    return `# [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
   } else if (license === "Mozilla Public License 2.0") {
-    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+    return `# [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
   } else if (license === "Apache 2.0") {
-    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    return `# [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
   } else {
     return " ";
   }
@@ -14,7 +14,17 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return `The work contained in this repository is covered by the MIT License. Learn about it [here](https://opensource.org/licenses/MIT)`;
+  } else if (license === "Mozilla Public License 2.0") {
+    return `The work contained in this repository is covered by the Mozilla Public License 2.0. Learn about it [here](https://opensource.org/licenses/MPL-2.0)`;
+  } else if (license === "Apache 2.0") {
+    return `The work contained in this repository is covered by the Apache 2.0 License. Learn about it [here](https://opensource.org/licenses/Apache-2.0)`;
+  } else {
+    return `No license associated with this repository.`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -23,7 +33,7 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  # ${renderLicenseBadge(data.license)}
+  ${renderLicenseBadge(data.license)}
 
   # Description
   ${data.description}
@@ -34,6 +44,7 @@ function generateMarkdown(data) {
   [Contributing](#contributing) \n
   [Tests](#tests) \n
   [Questions](#questions) \n
+  [License](#license)
 
 
   # Installation 
@@ -52,6 +63,9 @@ function generateMarkdown(data) {
   My Github username is ${data.github} \n
   Link to my Github profile: ${data.githubUrl} \n
   Or email me: ${data.email}
+
+  # License
+  ${renderLicenseLink(data.license)}
 
 
 `;
